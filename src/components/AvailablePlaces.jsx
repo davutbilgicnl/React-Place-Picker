26c4e5641213dd6493ef8b2440b74c13d1699339
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-01-10 14:10:50 +0100

    How NOT To send HTTP Request(And Why It's Wrong)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,6 +4,15 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  //This couse infinite loop. Because in every update to the setAvailablePlaces will couse rerender, and rerender will couse to run fetch
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+
   return (
     <Places
       title="Available Places"
