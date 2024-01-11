a8828478d35618efd381e9af32012f33c76a40b9
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-01-11 13:26:43 +0100

    Extracting Code & Improving Code Structure

---
+++ src/http.js
@@ -0,0 +1,12 @@
+// utility functions
+
+export async function fetchAvailablePlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to retch places");
+  }
+
+  return resData.places;
+}
