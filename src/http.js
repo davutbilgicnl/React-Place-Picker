db297f196704e16e4d27c20c9ef4d780c5a3daff
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-01-12 13:02:44 +0100

    Fetching user places data

--- src/http.js
+++ src/http.js
@@ -1,11 +1,20 @@
-// utility functions
-
 export async function fetchAvailablePlaces() {
   const response = await fetch("http://localhost:3000/places");
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error("Failed to retch places");
+    throw new Error("Failed to fetch places");
+  }
+
+  return resData.places;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
   }
 
   return resData.places;
@@ -14,7 +23,7 @@ export async function fetchAvailablePlaces() {
 export async function updateUserPlaces(places) {
   const response = await fetch("http://localhost:3000/user-places", {
     method: "PUT",
-    body: JSON.stringify({ places: places }),
+    body: JSON.stringify({ places }),
     headers: {
       "Content-Type": "application/json",
     },
